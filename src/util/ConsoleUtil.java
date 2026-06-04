64205cd56cef331af2f38df6023e94782b121fa1
Zhang San <zhangsan@student.com>
2026-06-04 11:38:06 +0800

    [Human] add console printing and formatting helpers

--- src/util/ConsoleUtil.java
+++ src/util/ConsoleUtil.java
@@ -1,4 +1,15 @@
 package util;
 
 public class ConsoleUtil {
+    public static void printDivider() {
+        System.out.println("========================================");
+    }
+    public static void printTitle(String title) {
+        printDivider();
+        System.out.println(title);
+        printDivider();
+    }
+    public static String formatPercent(double rate) {
+        return String.format("%.0f%%", rate * 100);
+    }
 }
